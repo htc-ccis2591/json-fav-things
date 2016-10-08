ed5a8b154efcbd14a2d28714237c7a777b169d91
brianandreasen1983 <bandreasen001@my.hennepintech.edu>
2016-10-08 16:06:39 -0500

    committing changes before major overhaul

--- js/prodscript.js
+++ js/prodscript.js
@@ -1,51 +1,56 @@
 //Start of the document
 $(document).ready(function () {
-    
+
     //Start of function scope for my favorite things.
-    
+
+    $('#addstuff').append('<button id="favoritethingbutton">Click here to see my favorite things</button>');
 
     var createMyFavThings = function (subject, description, videoID) {
         
         var favSubject = subject;
         var favDescription = description;
-        var favVideo = videoID;
-        var idSelector = "#addstuff"
+        var youTubeID = videoID;
         var headerID = "#" + subject;
-        var subjectHeader = "h2"
-        var subjectDescription = "p"
+        var favButton = "button"
+        var idSelector = "#addstuff";
+        var subjectHeader = "h2";
+        var subjectDescription = "p";
+        var favVideo = "iframe";
         
-
-        $(idSelector).append('<h2 id = " ' + subject + ' ">' + subject + '</h2>');
-        $(idSelector).append('<p>' + description + '</p>');
-        $(idSelector).append('<iframe src = "https://www.youtube.com/embed/' + videoID + '">' + videoID + '</iframe>');    
-        //$(subjectDescription).hide(); //When applied this is providing an in-line display to be overridden.
+        
+        $(idSelector).append('<h2 class = "' + subject + '">' + subject + '</h2>');
+        $(idSelector).append('<p class = "' + subject + '">' + subject + '</p>');
+        $(idSelector).append('<iframe class = "' + subject + '">' + subject + 'src = "https://www.youtube.com/embed/' + videoID + '"></iframe>');
+        
+        
+        $(subjectDescription).hide();
+        $(favVideo).hide();
+        $(subjectHeader).hide();
+        
+        $(favButton).click(function(){
+            $(this).remove();
+            $(subjectHeader).show();
+            
+        });
+        
+        //Troubleshoot more what is happening is that the subject is tied in with headerID with concatenation and is passing through the for each loop which is why it is showing all of the subject descriptions when the h2 is clicked.
+        
+        //We need to get it seperated so that when the user clicks on each h2 that it only shows content for that specific h2. 
+        
+        $(subjectHeader).click(function(){
+            $(subjectHeader).toggle();
+        });
+        
+        //JSON 
     }
-    
-        $('#addstuff').append('<button id="favoritethingbutton">Click here to see my favorite things</button>');
-    
 
-        $('button').click(function () {
-        $(this).remove();
-        $('h2').show();
-        //$('iframe').hide();
-        //$('p').hide();
-        
-    });
-    
-    //JSON 
-    
-    $.getJSON("myThings.json", function (data){
+    $.getJSON("myThings.json", function (data) {
         var item;
         console.log(data.myThings);
-        
-        //Start iterating through items to add them to the dom.
-        
-        $.each (data.myThings, function (index,item){
-            
+        $.each(data.myThings, function (index, item) {
             createMyFavThings(item.subject, item.description, item.videoID);
-            
-        }); //End of each loop iteration
-        
-    }); //End of JSON
-    
-});//end of document.ready function.
+        }); 
+    });
+    //Goal: I want to add a figcaption element after every iframe.
+    //$(subjectDescription).hide(); //When applied this is providing an in-line display to be overridden.
+}); //end of document.ready function.
