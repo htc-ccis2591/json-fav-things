633103d6a955b66380f01867d41adb0699a21b0c
brianandreasen1983 <bandreasen001@my.hennepintech.edu>
2016-10-09 01:03:08 -0500

    Trouble with the headers for some reason.

--- js/prodscript.js
+++ js/prodscript.js
@@ -1,56 +1,53 @@
 //Start of the document
 $(document).ready(function () {
+    //var addStuffHere = "#addstuff";
+    //var subjectHeaders = 'h2'
 
-    //Start of function scope for my favorite things.
 
-    $('#addstuff').append('<button id="favoritethingbutton">Click here to see my favorite things</button>');
 
     var createMyFavThings = function (subject, description, videoID) {
+        var favVideoID = "#" + videoID;
+
+        $('#addstuff').append('<div class="row">');
+        $('#addstuff').append('<div id= "' + videoID + '" class="col-md-6">');
+        $(favVideoID).append('<h2 id= "' + subject + '">' + subject + '</h2>');
+        $(favVideoID).append('<p>' + description + '</p>');
+        $("#addstuff").append('<div id="video ' + videoID + ' " class="col-md-6">');
+        $(favVideoID).append('<iframe src="https://www.youtube.com/embed/' + videoID + '"></iframe>');
+
+        $('p').hide();
+        $('iframe').hide();
+        $('h2').hide();
         
-        var favSubject = subject;
-        var favDescription = description;
-        var youTubeID = videoID;
-        var headerID = "#" + subject;
-        var favButton = "button"
-        var idSelector = "#addstuff";
-        var subjectHeader = "h2";
-        var subjectDescription = "p";
-        var favVideo = "iframe";
-        
-        
-        $(idSelector).append('<h2 class = "' + subject + '">' + subject + '</h2>');
-        $(idSelector).append('<p class = "' + subject + '">' + subject + '</p>');
-        $(idSelector).append('<iframe class = "' + subject + '">' + subject + 'src = "https://www.youtube.com/embed/' + videoID + '"></iframe>');
-        
-        
-        $(subjectDescription).hide();
-        $(favVideo).hide();
-        $(subjectHeader).hide();
-        
-        $(favButton).click(function(){
+        $('button').click(function () {
             $(this).remove();
-            $(subjectHeader).show();
-            
+            $('h2').show();
         });
         
-        //Troubleshoot more what is happening is that the subject is tied in with headerID with concatenation and is passing through the for each loop which is why it is showing all of the subject descriptions when the h2 is clicked.
-        
-        //We need to get it seperated so that when the user clicks on each h2 that it only shows content for that specific h2. 
-        
-        $(subjectHeader).click(function(){
-            $(subjectHeader).toggle();
+        $('<h2 id= "' + subject + '">' + subject + '</h2>').click(function(){
+            $('p').show();
+            $('iframe').show();
         });
-        
-        //JSON 
+
     }
+
+
+
+
+    $('h2').click(function () {
+        //$().show(); I need this to be the ID of the div's before H2 We want the ID to be the same name as the subject name as that is what is unique for our project.
+    });
+
+    /*var mySubjects = function (subject, description, videoID){
+    };*/
 
     $.getJSON("myThings.json", function (data) {
         var item;
-        console.log(data.myThings);
         $.each(data.myThings, function (index, item) {
             createMyFavThings(item.subject, item.description, item.videoID);
-        }); 
+        });
     });
-    //Goal: I want to add a figcaption element after every iframe.
-    //$(subjectDescription).hide(); //When applied this is providing an in-line display to be overridden.
-}); //end of document.ready function.
+
+    $('#addstuff').append('<button id="favoritethingbutton">Click here to see my favorite things</button>');
+
+});
