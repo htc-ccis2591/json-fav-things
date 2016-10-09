358dae637c9dbc1ad550f65c55f68acc45777aed
brianandreasen1983 <bandreasen001@my.hennepintech.edu>
2016-10-09 12:00:55 -0500

    Requesting feedback

--- js/prodscript.js
+++ js/prodscript.js
@@ -1,53 +1,79 @@
 //Start of the document
 $(document).ready(function () {
-    //var addStuffHere = "#addstuff";
-    //var subjectHeaders = 'h2'
+    var addStuffHere = "#addstuff";
+    var subjectHeaders = 'h2';
 
 
-
-    var createMyFavThings = function (subject, description, videoID) {
+    var createMyFavThings = function (subject, description, videoID, myClass) {
         var favVideoID = "#" + videoID;
 
-        $('#addstuff').append('<div class="row">');
-        $('#addstuff').append('<div id= "' + videoID + '" class="col-md-6">');
-        $(favVideoID).append('<h2 id= "' + subject + '">' + subject + '</h2>');
-        $(favVideoID).append('<p>' + description + '</p>');
-        $("#addstuff").append('<div id="video ' + videoID + ' " class="col-md-6">');
-        $(favVideoID).append('<iframe src="https://www.youtube.com/embed/' + videoID + '"></iframe>');
+        $(addStuffHere).append('<div class="row">');
+        $(addStuffHere).append('<div id= "' + videoID + '" class="col-md-6">');
+        $(favVideoID).append('<h2 id="' + subject + '">' + subject + '</h2>')
+        $(subjectHeaders).hide();
 
+        $(favVideoID).append('<p class="' + myClass + '">' + description + '</p>');
         $('p').hide();
+
+        $(addStuffHere).append('<div id="video ' + videoID + ' " class="col-md-6">');
+        $(favVideoID).append('<iframe class="' + myClass + '" src="https://www.youtube.com/embed/' + videoID + '"></iframe>');
         $('iframe').hide();
-        $('h2').hide();
-        
-        $('button').click(function () {
-            $(this).remove();
-            $('h2').show();
-        });
-        
-        $('<h2 id= "' + subject + '">' + subject + '</h2>').click(function(){
-            $('p').show();
-            $('iframe').show();
+
+
+        $('#Programming').click(function () {
+            $('.softwaredev').show();
+            $('.compgaming').hide();
+            $('.musiccontent').hide();
+            $('.andreasenfamily').hide();
+            $('.luxury').hide();
         });
 
+        $('#Gaming').click(function () {
+            $('.compgaming').show();
+            $('.softwaredev').hide();
+            $('.musiccontent').hide();
+            $('.andreasenfamily').hide();
+            $('.luxury').hide();
+        });
+
+        $('#Music').click(function () {
+            $('.musiccontent').show();
+            $('.compgaming').hide();
+            $('.softwaredev').hide();
+            $('.andreasenfamily').hide();
+            $('.luxury').hide();
+        });
+
+        $('#Family').click(function () {
+            $('.andreasenfamily').show();
+            $('.softwaredev').hide();
+            $('.compgaming').hide();
+            $('.musiccontent').hide();
+            $('.luxury').hide();
+        });
+
+        $('#Autos').click(function () {
+            $('.luxury').show();
+            $('.softwaredev').hide();
+            $('.compgaming').hide();
+            $('.musiccontent').hide();
+            $('.andreasenfamily').hide();
+        });
     }
-
-
-
-
-    $('h2').click(function () {
-        //$().show(); I need this to be the ID of the div's before H2 We want the ID to be the same name as the subject name as that is what is unique for our project.
-    });
-
-    /*var mySubjects = function (subject, description, videoID){
-    };*/
 
     $.getJSON("myThings.json", function (data) {
         var item;
         $.each(data.myThings, function (index, item) {
-            createMyFavThings(item.subject, item.description, item.videoID);
+            createMyFavThings(item.subject, item.description, item.videoID, item.myClass);
         });
     });
 
     $('#addstuff').append('<button id="favoritethingbutton">Click here to see my favorite things</button>');
+
+    $('button').click(function () {
+        $(this).remove();
+        $(subjectHeaders).show();
+    });
+
 
 });
