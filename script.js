a251385af038bfa5e2a618926c628a223c3707f6
ChrisMyster <infiniteaxis@gmail.com>
2016-10-11 01:37:17 -0500

    version 4

--- script.js
+++ script.js
@@ -2,13 +2,10 @@
 $(function () {
 	"use strict";
 
-
 	var myHeading = function () {
-		//myFavorites = ('#favorites');
+
 		$("#favorites").before("<h1>Click here to see a few of my favorite things</h1>");
 		$("#favorites").addClass("hide");
-
-
 		$('h1').on('click', function () {
 			$("#favorites").slideDown(2000);
 			console.log("you clicked");
@@ -16,63 +13,33 @@ $(function () {
 		});
 
 	};
-	myHeading();
-
-
-
-	var myWrap = function () {
-		$('h3[1], p[1], img[1]').wrap('div class="containerDiv"></div>');
-	};
-
-
-	//$("p, img").addClass("hide");
-	//create favorites object
-	//var $secFavs = $('div#favorites');
-	//$secFavs.hide();
-
-	//create images object
-	//var $favImg = $('img');
-	//$favImg.hide();
-
 
 	var createMyFavorites = function (thing, image, info) {
 
-		var favorites = ("#favorites");
-		//$(favorites).before$('<div id ="' + thing + '">');
+		var $favorites = $("#favorites");
 
-		$(favorites).append("<h3>" + thing + "</h3>");
-		$(favorites).append('<img src=" ' + image + '">');
-		$(favorites).append("<p>" + info + "</p>");
+		$favorites.append('<section id="' + thing + '">');
 
-		$('img').css("display", "block");
-		$('img').css("margin", "auto");
-
-		$('p').css("color", "#D4CA6A");
-
+		$("#" + thing).append("<h3>" + thing + "</h3>");
+		$("#" + thing).append('<img src=" ' + image + '">');
+		$("#" + thing).append("<p>" + info + "</p>");
 	};
 
-	//$("img, p").addClass("hide");
-	//$(favorites).after("<div>");
-	//console.log(createMyFavorites);
-
+	myHeading();
 
 	$.getJSON("data.json", function (data) {
 		$.each(data.favorites, function (index, item) {
 			createMyFavorites(item.thing, item.image, item.info);
 		});
 		$('h3').on('click', function () {
-			$("p, img").toggle();
+			$(this).siblings().toggle();
 			console.log("you clicked");
+
 		});
+		$('img').css("display", "block");
+		$('img').css("margin", "auto");
+		$('p').css("color", "#D4CA6A");
+
 	});
-
-	/*$('img').each(function () {
-		var $img = $(this);
-		var filename = $img.attr('src')
-		$img.attr('alt', filename.substring(0, filename.lastIndexOf('.')));
-	});*/
-	//myWrap();
-
-
 
 });
