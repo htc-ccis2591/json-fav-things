888d5ba1d93d30bfe16b9b39c787452422737c83
ChrisMyster <infiniteaxis@gmail.com>
2016-10-12 03:47:45 -0500

    v5

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ $(function () {
 
 	var myHeading = function () {
 
-		$("#favorites").before("<h1>Click here to see a few of my favorite things</h1>");
+		$("#favorites").before("<header><h1>Click here to see a few of my favorite things</h1></header>");
 		$("#favorites").addClass("hide");
 		$('h1').on('click', function () {
-			$("#favorites").slideDown(2000);
-			console.log("you clicked");
+			$("#favorites").slideDown(4000);
 			$("p, img").hide();
-		});
 
+
+		});
 	};
 
 	var createMyFavorites = function (thing, image, info) {
@@ -23,6 +23,7 @@ $(function () {
 		$("#" + thing).append("<h3>" + thing + "</h3>");
 		$("#" + thing).append('<img src=" ' + image + '">');
 		$("#" + thing).append("<p>" + info + "</p>");
+
 	};
 
 	myHeading();
@@ -32,13 +33,15 @@ $(function () {
 			createMyFavorites(item.thing, item.image, item.info);
 		});
 		$('h3').on('click', function () {
-			$(this).siblings().toggle();
-			console.log("you clicked");
+			$(this).siblings().toggle(500);
 
 		});
 		$('img').css("display", "block");
 		$('img').css("margin", "auto");
 		$('p').css("color", "#D4CA6A");
+
+		//$("#Literature:nth-child(3)").replaceWith("<h3>Classic Literature</h3>");
+
 
 	});
 
